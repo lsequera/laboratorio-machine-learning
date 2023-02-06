fcd31fce4d2d1de8f2f3786860efafda06575272
Ling <lsequera@proton.me>
2023-02-05 20:41:59 -0500

    little experiment

--- app.py
+++ app.py
@@ -13,6 +13,18 @@ column_equivalence = pickle.load(open("churn/models/column_equivalence.pk", "rb"
 app = Flask(__name__)
 
 def convert_numerical(features):
+    """
+    Converts the list of features from string to numerical values using the method to_numeric() of the module pandas
+
+    Parameters
+    ----------
+    features : List[str]
+        List with the following parameters in the same order: [CreditScore, Geography, Gender, Age, Tenure, Balance, NumOfProducts, HasCrCard, IsActiveMember, EstimatedSalary]
+
+        Geograpy parameter can be one of the set: {France, Germany, Spain}
+        Gender paramenter can be one of the set: {Female, Male}
+    
+    """
     output = []
     for i, feat in enumerate(features):
         if i in column_equivalence:
@@ -24,6 +36,8 @@ def convert_numerical(features):
                 output.append(0)
     return output
 
+
+#This 
 @app.route('/query')
 def query_example():
     features = convert_numerical(request.args.get('feats').split(','))
